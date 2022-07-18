3929e0e5c8be211f9b9777e3a1aa1f354f95f933
Berzan <berzan@tutanota.com>
2022-07-18 14:11:38 +0300

    add: Mint struct & mint function

--- programs/nft-minting-dapp/src/lib.rs
+++ programs/nft-minting-dapp/src/lib.rs
@@ -1,5 +1,5 @@
 use anchor_lang::{prelude::*, solana_program::program::invoke, system_program};
-use anchor_spl::{associated_token, mint};
+use anchor_spl::{associated_token, mint, token};
 use mpl_token_metadata::{ID as TOKEN_METADATA_ID, instruction as token_instruction};
 
 
@@ -9,10 +9,35 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod nft_minting_dapp {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn mint(ctx: Context<Mint>) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Mint<'info> {
+    /// CHECK: This account will be created with Metaplex
+    #[account(mut)]
+    pub metadata: UncheckedAccount<'info>,
+
+    /// CHECK: This account will also be created with Metaplex
+    #[account(mut)]
+    pub master_edition: UncheckedAccount<'info>,
+
+    #[account(mut)]
+    pub mint: Signer<'info>,
+
+    /// CHECK: This account will be created with Anchor
+    #[account(mut)]
+    pub token_account: UncheckedAccount<'info>,
+
+    #[account(mut)]
+    pub mint_authority: Signer<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, token::Token>,
+    pub associated_token_program: Program<'info, associated_token::AssociatedToken>,
+
+    // CHECK: This program will be checked by Metaplex
+    pub token_metadata_program: UncheckedAccount<'info>,
+}
