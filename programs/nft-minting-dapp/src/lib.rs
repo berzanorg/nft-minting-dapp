119d5ab5b5cd4ae72807235b8820309b8f5d8cb2
Berzan <berzan@tutanota.com>
2022-07-18 14:22:56 +0300

    add: content to mint fn & add: CHECK for token_metadata_program

--- programs/nft-minting-dapp/src/lib.rs
+++ programs/nft-minting-dapp/src/lib.rs
@@ -1,4 +1,4 @@
-use anchor_lang::{prelude::*, solana_program::program::invoke, system_program};
+use anchor_lang::{prelude::*, solana_program::program::invoke};
 use anchor_spl::{associated_token, mint, token};
 use mpl_token_metadata::{ID as TOKEN_METADATA_ID, instruction as token_instruction};
 
@@ -10,6 +10,26 @@ pub mod nft_minting_dapp {
     use super::*;
 
     pub fn mint(ctx: Context<Mint>) -> Result<()> {
+        msg!("Creating a mint account...");
+        msg!("Mint: {}", &ctx.accounts.mint.key);
+
+        msg!("Initializing the mint account...");
+        msg!("Mint: {}", &ctx.accounts.mint.key);
+
+        msg!("Creating a token account...");
+        msg!("Token Address: {}", &ctx.accounts.token_account.key);
+
+        msg!("Minting token to the token account...");
+        msg!("Mint: {}", &ctx.accounts.mint.to_account_info().key);
+        msg!("Token Address: {}", &ctx.accounts.token_account.key);
+
+        msg!("Creating a metadata account...");
+        msg!("Metadata Account Address: {}", &ctx.accounts.metadata.to_account_info().key);
+
+        msg!("Creating a master edition metadata account...");
+        msg!("Master Edition Metadata Account Address: {}", &ctx.accounts.master_edition.to_account_info().key);
+
+        msg!("Token Minting process is completed successfully...");
         Ok(())
     }
 }
@@ -38,6 +58,6 @@ pub struct Mint<'info> {
     pub token_program: Program<'info, token::Token>,
     pub associated_token_program: Program<'info, associated_token::AssociatedToken>,
 
-    // CHECK: This program will be checked by Metaplex
+    /// CHECK: This program will be checked by Metaplex
     pub token_metadata_program: UncheckedAccount<'info>,
 }
